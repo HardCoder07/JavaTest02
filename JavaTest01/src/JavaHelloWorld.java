9d5817a1ef87898cb949c45172bdd617dff82f22
Gamer <Gamer@DESKTOP-R1O8QU2.home>
2016-12-04 15:50:03 -0500

    5:win

--- JavaTest01/src/JavaHelloWorld.java
+++ JavaTest01/src/JavaHelloWorld.java
@@ -5,6 +5,8 @@ public class JavaHelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World 12345");
 		System.out.println("Hello World 12345");
+		System.out.println("Hello World 12345");
+		System.out.println("Hello World 12345");
 	}
 
 }
