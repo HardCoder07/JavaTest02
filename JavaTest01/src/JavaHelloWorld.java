3d2a876ae8f0bb5139c47527ffd76937d8f622d1
Gamer <Gamer@DESKTOP-R1O8QU2.home>
2016-12-03 22:06:53 -0500

    Try2: Check 02 from windows

--- JavaTest01/src/JavaHelloWorld.java
+++ JavaTest01/src/JavaHelloWorld.java
@@ -3,7 +3,7 @@ public class JavaHelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World 321");
+		System.out.println("Hello World 3210");
 	}
 
 }
